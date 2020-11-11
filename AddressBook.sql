0ad77fe5da68e8cf712f3d80370d3fd0bc60fd0f
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-11-11 20:04:36 +0530

    ADDED:createTable to createthe addressbook table

--- AddressBook.sql
+++ AddressBook.sql
@@ -1,3 +1,12 @@
 CREATE DATABASE Address_Book_Service;
-
-
+USE Address_Book_Service;
+CREATE TABLE AddressBook(
+    FirstName varchar(100) NOT NULL PRIMARY KEY,
+    LastName varchar(255) NOT NULL,
+	Address varchar(255) NOT NULL,
+    City varchar(255) NOT NULL,
+    State varchar(255)NOT NUll,
+    Zip int(6) NULL NUll,
+    MobileNumber bigint(10)NOT NULL,
+    EmailId Varchar(320) NOT NULL
+);
