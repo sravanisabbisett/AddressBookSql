1a23e77605be7da5954a607c623cca6bd801ade3
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-11-11 20:09:01 +0530

    ADDED:Insert query to insert the data in the data

--- AddressBook.sql
+++ AddressBook.sql
@@ -1,5 +1,6 @@
 CREATE DATABASE Address_Book_Service;
 USE Address_Book_Service;
+
 CREATE TABLE AddressBook(
     FirstName varchar(100) NOT NULL PRIMARY KEY,
     LastName varchar(255) NOT NULL,
@@ -10,3 +11,13 @@ CREATE TABLE AddressBook(
     MobileNumber bigint(10)NOT NULL,
     EmailId Varchar(320) NOT NULL
 );
+
+DROP TABLE AddressBook;
+
+INSERT INTO AddressBook(FirstName,LastName,Address,City,State,Zip,MobileNumber,EmailId)
+VALUES ('Sravani','Sabbisetti','GandhiChowk','Bantumilli','AndhraPradesh',521324,8712443377,'sravani@gmail.com'),
+('Rupika','Sabbisetti','AronodayaColony','Hyderabad','Telangana',500081,9295702642,'abc@gmail.com'),
+('Saraiya','Purkana','Krths','Bantumilli','AndhraPradesh',123456,4567890123,'saru@gmail.com'),
+('Manju','Chedhalla','Ponnuru','Guntur','AndhraPradesh',678901,9876543210,'manju@gmail.com'),
+('Akhilesh','Krishna','Perungudi','Chennai','Tamilnadu',876904,9765432167,'akhi@gmail.com');
+
